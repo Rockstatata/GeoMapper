d405be3a3c94fba31914fb66161781b6e9f7bdee
Rockstatata <sarwad015@gmail.com>
2025-10-12 05:25:57 +0600

    feat: reorganize project structure by moving files into Geomapper directory and initiated a local web server
    - Moved all project files into a new directory named 'Geomapper' for better organization.
    - Set up a basic local web server to facilitate development and testing.
    - Updated project documentation to reflect the new structure and server setup.
    - This restructuring aims to enhance project maintainability and streamline future development efforts.
    - The local web server will allow for easier testing and debugging of the Geomapper application.

--- Geomapper/Geomapper.ino
+++ Geomapper/Geomapper.ino
@@ -1,4 +1,6 @@
 #include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <WebServer.h>   
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BT_SPP_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to enable it
@@ -15,12 +17,27 @@
 #define left_motor_backward 4
 #define left_motor_speed 13
 
+const char* ssid = "Blue";
+const char* password = "11235813";
+extern float front_distance, left_distance, right_distance;
+
+WebServer server(80);
+
 const int TRIG_F = 32;   // Forward sensor
 const int ECHO_F = 39;
 const int TRIG_L = 25;   // Left sensor
 const int ECHO_L = 34;
 const int TRIG_R = 33;  // Right sensor
 const int ECHO_R = 36;
+
+void moveForward();
+void moveBackward();
+void turnLeft();
+void turnRight();
+void stopCar();
+
+// simple mode tracker
+String mode = "autonomous";
 
 
 BluetoothSerial SerialBT;
@@ -77,9 +94,64 @@ void setup() {
 
   Serial.begin(115200);
   SerialBT.begin();
+  connectToWiFi();
+  setupWebServer();
 }
 
+void connectToWiFi() {
+  Serial.println("\n[Connecting to WiFi]");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupWebServer() {
+  // serve current sensor data
+  server.on("/status", HTTP_GET, []() {
+    String json = "{";
+    json += "\"front\":" + String(front_distance) + ",";
+    json += "\"left\":" + String(left_distance) + ",";
+    json += "\"right\":" + String(right_distance) + ",";
+    json += "\"mode\":\"" + String(mode) + "\"";
+    json += "}";
+    server.send(200, "application/json", json);
+  });
+
+  // control car remotely
+  server.on("/move", HTTP_GET, []() {
+    if (server.hasArg("dir")) {
+      String dir = server.arg("dir");
+      if (dir == "forward") moveForward();
+      else if (dir == "backward") moveBackward();
+      else if (dir == "left") turnLeft();
+      else if (dir == "right") turnRight();
+      else stopCar();
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(400, "text/plain", "Missing ?dir= parameter");
+    }
+  });
+
+  // simple homepage
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html",
+      "<html><body><h3>Smart IoT Car</h3><p><a href='/status'>Status JSON</a></p>"
+      "<p>Use /move?dir=forward|left|right|backward|stop</p></body></html>");
+  });
+
+  server.begin();
+  Serial.println("WebServer started.");
+}
+
+
 void loop() {
+  server.handleClient();
   digitalWrite(LED_PIN, ledstate);
   bluetooth();
   while(ledstate){
---
+++ Geomapper/bluetooth_reading.ino
@@ -0,0 +1,26 @@
+void bluetooth(){
+  if(SerialBT.available()  > 0){
+    x = SerialBT.read();
+    Serial.println(x);
+    if(x == 'F' && !ledstate) motor(spd, spd);
+    else if(x == 'B' && !ledstate) motor(-spd, -spd);
+    else if(x == 'R' && !ledstate) motor(-spd, spd);
+    else if(x == 'L' && !ledstate) motor(spd, -spd);
+    else if(x == 'I' && !ledstate) motor(0, spd);
+    else if(x == 'G' && !ledstate) motor(spd, 0);
+    else if(x == 'J' && !ledstate) motor(0, -spd);
+    else if(x == 'H' && !ledstate) motor(-spd, 0);
+    else if(x == 'S' && !ledstate) motor(0, 0);
+    else if(x == '0') spd = 0;
+    else if(x == '1') spd = 25;
+    else if(x == '2') spd = 50;
+    else if(x == '3') spd = 75;
+    else if(x == '4') spd = 100;
+    else if(x == '5') spd = 125;
+    else if(x == '6') spd = 150;
+    else if(x == '7') spd = 175;
+    else if(x == '8') spd = 200;
+    else if(x == '9') spd = 225;
+    else if(x == 'q') spd = 255;
+  }
+}
